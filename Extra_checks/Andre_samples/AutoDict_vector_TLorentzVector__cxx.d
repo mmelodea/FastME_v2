58fe4612af5a1281fbe484ea02addab269ba4c92
mmelodea <mmelodea@cern.ch>
2015-10-27 11:09:15 -0200

    Including aditional tests...

---
+++ Extra_checks/Andre_samples/AutoDict_vector_TLorentzVector__cxx.d
@@ -0,0 +1,40 @@
+
+# DO NOT DELETE
+
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TLorentzVector.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMath.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/Rtypes.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/RConfig.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/RVersion.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/DllImport.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/Rtypeinfo.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/snprintf.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/strlcpy.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TGenericClassInfo.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TSchemaHelper.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMathBase.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TError.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TVector3.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TVector2.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TObject.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TStorage.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TVersionCheck.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/Riosfwd.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TBuffer.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMatrix.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMatrixF.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMatrixT.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMatrixTBase.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TNamed.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TString.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMatrixFBasefwd.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMatrixDBasefwd.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TVectorFfwd.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TVectorDfwd.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMatrixTUtils.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMatrixFfwd.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMatrixFUtils.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TMatrixFUtilsfwd.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT/include/root/TRotation.h
+AutoDict_vector_TLorentzVector__cxx.so: /home/sabayon/ROOT//include/root/cintdictversion.h /home/sabayon/ROOT//include/root/RVersion.h
+AutoDict_vector_TLorentzVector__cxx__ROOTBUILDVERSION= 5.32/04
